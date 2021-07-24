2f46e7acca62e8a7cb076ae4c747ec475d1f19bd
DanielleEmely <danielleemily92014@gmail.com>
2021-07-24 18:08:14 -0300

    Aula 3 e 4, com alterações na conexão com o Dato

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
 import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
@@ -22,9 +22,21 @@ function ProfileSidebar(propriedades) {
   )
 }
 
+function ProfileRelationsBox(propriedades){
+    return (
+      <ProfileRelationsBoxWrapper>
+        <h2 className="smallTitle">
+        {propriedades.title} ({propriedades.items.length})
+      </h2>
+      <ul></ul>
+      </ProfileRelationsBoxWrapper>
+    )
+
+}
+
 export default function Home() {
   const usuarioAleatorio = 'DanielleEmely';
-  const [comunidades, setComunidades] = React.useState([{
+  {/*const [comunidades, setComunidades] = React.useState([{
     id: '12802378123789378912789789123896123', 
     title: 'Loucos por Pets',
     image: 'https://i.pinimg.com/736x/dc/c6/6a/dcc66a7ef2f74e81da4fdf3f1b52ab81.jpg',
@@ -42,7 +54,9 @@ export default function Home() {
     image: 'https://img.freepik.com/free-vector/cute-welsh-corgi-dog-waving-paw-cartoon_42750-623.jpg?size=338&ext=jpg',
     src: 'https://www.reddit.com/r/dogpictures/'
   }
-]);
+]);*/}
+const [comunidades, setComunidades] = React.useState([]);
+
   const pessoasFavoritas = [
     'iagoizi',
     'juunegreiros',
@@ -51,6 +65,43 @@ export default function Home() {
     'marcobrunodev',
     'felipefialho'
   ]
+
+  const [seguidores, setSeguidores] = React.useState([]);
+  React.useEffect(function() {
+    fetch(`https://api.github.com/users/${usuarioAleatorio}/followers`)
+    .then(function (respostaDoServidor) {
+      return respostaDoServidor.json();
+    })
+    .then(function(respostaCompleta) {
+      setSeguidores(respostaCompleta);
+    })
+     // API GraphQL
+     fetch('https://graphql.datocms.com/', {
+      method: 'POST',
+      headers: {
+        'Authorization': '3a0ec32edb24d73ede3733750c97a4',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({ "query": `query {
+        allCommunities {
+          id 
+          title
+          image
+          src
+          slug
+        }
+      }` })
+    })
+    .then((response) => response.json()) // Pega o retorno do response.json() e já retorna
+    .then((respostaCompleta) => {
+      const comunidadesVindasDoDato = respostaCompleta.data.allCommunities;
+      console.log(comunidadesVindasDoDato)
+      setComunidades(comunidadesVindasDoDato)
+    })
+
+  }, [])
+
 
   return (
     <>
@@ -79,13 +130,27 @@ export default function Home() {
                 console.log('Campo: ', dadosDoForm.get('image'));
                 {/* Adicionada link para comunidade de forma  automatica*/}
                 const comunidade = {
-                  id: new Date().toISOString(),
                   title: dadosDoForm.get('title'),
                   image: dadosDoForm.get('image'),
-                  src: dadosDoForm.get('src')
+                  src: dadosDoForm.get('src'),
+                  slug: usuarioAleatorio
                 }
-                const comunidadesAtualizadas = [...comunidades, comunidade];
-                setComunidades(comunidadesAtualizadas)
+               
+                fetch('/api/comunidades', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify(comunidade)
+                })
+                .then(async (response) => {
+                  const dados = await response.json();
+                  console.log(dados.registroCriado);
+                  const comunidade = dados.registroCriado;
+                  const comunidadesAtualizadas = [...comunidades, comunidade];
+                  setComunidades(comunidadesAtualizadas)
+                })
+
             }}>
               <div>
                 <input
@@ -119,7 +184,9 @@ export default function Home() {
 
         </div>
         <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
-        
+
+        <ProfileRelationsBox title="Seguidores" items={seguidores} />
+
         <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
               Comunidades ({comunidades.length})
@@ -128,7 +195,7 @@ export default function Home() {
               {comunidades.map((itemAtual) => {
                 return (
                   <li key={itemAtual.id}>
-                    <a href={itemAtual.src}>
+                   <a href={itemAtual.src}>
                       <img src={itemAtual.image} />
                       <span>{itemAtual.title}</span>
                     </a>
